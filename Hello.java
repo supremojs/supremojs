12fbd620802df92087f2c0b73342065e7a560210
justin <07302290@dwc-legazpi.com>
2024-01-31 12:05:43 +0800

    Initial commit

---
+++ Hello.java
@@ -0,0 +1,14 @@
+public class Hello 
+{
+    /* 
+     *Author : Justin B. Saligumba
+     *Date : January 25, 2024
+     *Project : Activity 1 - Hello World 
+     *
+     * Note : Do Not Modify or Delete this, THANK YOU! 
+    */
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello World!");   
+    }   
+}
---
+++ VariableExample.java
@@ -0,0 +1,11 @@
+public class VariableExample {
+    public static void main(String[] args) {
+        int x = 5;
+        double y = 7.5;
+        String message = "Hello, World!";
+        
+        System.out.println("x: " + x);
+        System.out.println("y: " + y);
+        System.out.println("message: " + message);
+    }
+}
